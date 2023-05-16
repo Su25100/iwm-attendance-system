08c471d64ca02002e67b9695bfdc28fc28d6952c
su25100 <surbhibanke2599@gmail.com>
2023-05-16 15:28:30 +0530

    first commit login page

--- depthead/urls.py
+++ depthead/urls.py
@@ -3,4 +3,5 @@ from .import views
 urlpatterns = [
     
     path('login/',views.login),
+    path('login/authentication',views.login_view),
 ]
--- depthead/views.py
+++ depthead/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
     return render(request,'dept/login.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after successful login
+        else:
+            messages.error(request, 'Invalid username or password')  # Display an error message
+
+    return render(request, 'login.html')
